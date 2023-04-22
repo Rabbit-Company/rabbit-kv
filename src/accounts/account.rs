3e9c12daa917757dcfeab648879e535cfcbfccf5
Ziga <ziga.zajc007@gmail.com>
2023-04-22 09:43:18 +0200

    Update

--- src/accounts/account.rs
+++ src/accounts/account.rs
@@ -1,9 +1,17 @@
 use std::collections::HashMap;
 
-use crate::caches::cache::Cache;
+use crate::caches::cache;
 
 pub struct Account {
 	pub username: String,
 	pub token: String,
-	pub caches: HashMap<String, Cache>
+	pub caches: HashMap<String, cache::Cache>
+}
+
+impl Account {
+
+	pub fn new(username: String, token: String) -> Self {
+		Account { username, token, caches: HashMap::new() }
+	}
+
 }
