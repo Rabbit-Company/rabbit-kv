032d58e0fbbf819a2c20bef372a27abee5b55ac1
Ziga Zajc <ziga.zajc007@gmail.com>
2024-04-30 17:59:24 +0200

    Update

--- src/endpoints/v1/get.rs
+++ src/endpoints/v1/get.rs
@@ -13,7 +13,7 @@ pub async fn handle_get(
 	TypedHeader(bearer_token): TypedHeader<Authorization<Bearer>>
 ) -> impl IntoResponse{
 
-  if state.token.clone().ne(bearer_token.token()) {
+  if state.token.ne(bearer_token.token()) {
     return Json(Error{ code: 1000, message: "Provided token is incorrect!".to_string() }).into_response();
   }
 
--- src/endpoints/v1/set.rs
+++ src/endpoints/v1/set.rs
@@ -21,7 +21,7 @@ pub async fn handle_get(
 	TypedHeader(bearer_token): TypedHeader<Authorization<Bearer>>
 ) -> impl IntoResponse{
 
-  if state.token.clone().ne(bearer_token.token()) {
+  if state.token.ne(bearer_token.token()) {
     return Json(Error{ code: 1000, message: "Provided token is incorrect!".to_string()}).into_response();
   }
 
@@ -37,7 +37,7 @@ pub async fn handle_post(
 	Json(payload): Json<Payload>
 ) -> impl IntoResponse{
 
-  if state.token.clone().ne(bearer_token.token()) {
+  if state.token.ne(bearer_token.token()) {
     return Json(Error{ code: 1000, message: "Provided token is incorrect!".to_string() }).into_response();
   }
 
